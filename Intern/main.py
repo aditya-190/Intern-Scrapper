2b6e4af2f398a555cce912a51aeac3428e6e3b35
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-02-22 10:41:12 +0530

    Some More Modularisation for Ktor integration.

---
+++ Intern/main.py
@@ -0,0 +1,3 @@
+import spiders.Linkedin as linkedinSpider
+
+linkedinSpider.main(number_of_pages=1, keywords="android", location="India")
--- Intern/spiders/Linkedin.py
+++ Intern/spiders/Linkedin.py
@@ -1,12 +1,12 @@
 import json
 import os
+import re
 import dateparser
 import html2text
-import re
-import scrapy
 import requests
-from Intern.Intern.items import InternItem
+import scrapy
 from scrapy.crawler import CrawlerProcess
+from Intern.Intern.items import InternItem
 
 
 def next_page(response):
@@ -23,62 +23,72 @@ def next_page(response):
     items['jobDuration'] = "Nothing Here"
     items['aboutCompany'] = "Nothing Here"
 
-    applyLink = response.css(".top-card-layout__cta--primary").xpath("@href").extract_first()
-    if not applyLink:
+    apply_link = response.css(".top-card-layout__cta--primary").xpath("@href").extract_first()
+    if not apply_link:
         items['applyNowPage'] = response.css(".topcard__link").xpath("@href").extract_first().strip()
     else:
-        items['applyNowPage'] = applyLink.strip()
+        items['applyNowPage'] = apply_link.strip()
 
-    htmlParser = html2text.HTML2Text()
-    htmlParser.ignore_links = False
-    htmlParser.ignore_emphasis = True
-    htmlParser.ul_item_mark = "\u2022 "
-    htmlParser.strong_mark = ""
-    htmlParser.emphasis_mark = ""
-    htmlParser.strong_mark = ""
+    html_parser = html2text.HTML2Text()
+    html_parser.ignore_links = False
+    html_parser.ignore_emphasis = True
+    html_parser.ul_item_mark = "\u2022 "
+    html_parser.strong_mark = ""
+    html_parser.emphasis_mark = ""
+    html_parser.strong_mark = ""
 
-    rawDescription = "".join(response.css(".show-more-less-html__markup--clamp-after-5").extract()).strip()
-    items['postDescription'] = htmlParser.handle(rawDescription)
+    raw_description = "".join(response.css(".show-more-less-html__markup--clamp-after-5").extract()).strip()
+    items['postDescription'] = html_parser.handle(raw_description)
     items['jobRequirement'] = items['postDescription']
     items['jobEligibility'] = items['postDescription']
 
     yield items
 
 
+def parse(response):
+    data_entity_urn = response.css(".base-card").xpath("@data-entity-urn").extract()
+    data_search_id = response.css(".base-card").xpath("@data-search-id").extract()
+    data_tracking_id = response.css(".base-card").xpath("@data-tracking-id").extract()
+
+    for i in range(0, len(data_entity_urn)):
+        current_job_posting_id = re.sub("urn:li:jobPosting:", "", data_entity_urn[i])
+        current_data_search_id = data_search_id[i]
+        current_data_tracking_id = data_tracking_id[i]
+
+        next_page_url = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}?refId={}%3D%3D&trackingId={}%3D%3D".format(
+            current_job_posting_id, current_data_search_id, current_data_tracking_id)
+
+        yield scrapy.Request(url=next_page_url, callback=next_page)
+
+
 class LinkedinSpider(scrapy.Spider):
     name = "linkedin"
 
-    def start_requests(self, jobKeyWord="Kotlin", location="India", numberOfPages=1):
-        for pages in range(0, numberOfPages * 25, 25):
+    def __init__(self, number_of_pages=1, keywords="", location="", **kwargs):
+        super().__init__(**kwargs)
+        self.number_of_pages = number_of_pages
+        self.keywords = keywords
+        self.location = location
+
+    def start_requests(self):
+        for pages in range(0, self.number_of_pages * 25, 25):
             yield scrapy.Request(
                 url='https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?start={}&keywords={}&location={}'.format(
-                    pages, jobKeyWord.strip().replace(" ", "%20"), location.strip().replace(" ", "%20")),
-                callback=self.parse)
-
-    def parse(self, response, **kwargs):
-        dataEntityUrn = response.css(".base-card").xpath("@data-entity-urn").extract()
-        dataSearchId = response.css(".base-card").xpath("@data-search-id").extract()
-        dataTrackingId = response.css(".base-card").xpath("@data-tracking-id").extract()
-
-        for i in range(0, len(dataEntityUrn)):
-            currentJobPostingId = re.sub("urn:li:jobPosting:", "", dataEntityUrn[i])
-            currentDataSearchId = dataSearchId[i]
-            currentDataTrackingId = dataTrackingId[i]
-
-            nextPageUrl = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{}?refId={}%3D%3D&trackingId={}%3D%3D".format(
-                currentJobPostingId, currentDataSearchId, currentDataTrackingId)
-
-            yield scrapy.Request(url=nextPageUrl, callback=next_page)
+                    pages, self.keywords.strip().replace(" ", "%20"), self.location.strip().replace(" ", "%20")),
+                callback=parse)
 
 
 def send_data():
-    baseUrl = "https://aditya-intern-backend.herokuapp.com/job/all"
+    production_url = "https://aditya-intern-backend.herokuapp.com/job/all"
+    development_url = "http://0.0.0.0:8080/job/all"
+
+    base_url = development_url
     headers = {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Bearer {ADD YOUR TOKEN HERE}"
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwL2hlbGxvIiwiaXNzIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJlbWFpbCI6ImFhZGkuYmJoYXJkd2FqQGdtYWlsLmNvbSJ9.3jdo9WUyeASv9GbTWHjRPjLrk5sg0cCKgzcMcC5EF4w"
     }
-    jsonData = json.load(open('output.json'))
-    response = requests.post(baseUrl, headers=headers, json=jsonData)
+    json_data = json.load(open('output.json'))
+    response = requests.post(base_url, headers=headers, json=json_data)
 
     if response.status_code == 200:
         print("Data Inserted Successfully")
@@ -86,8 +96,7 @@ def send_data():
         print("Something Went Wrong.")
 
 
-if __name__ == "__main__":
-
+def main(number_of_pages, keywords, location):
     if os.path.exists("output.json"):
         os.remove("output.json")
 
@@ -99,6 +108,6 @@ if __name__ == "__main__":
         'DOWNLOAD_DELAY': '1'
     })
 
-    process.crawl(LinkedinSpider)
+    process.crawl(LinkedinSpider, number_of_pages=number_of_pages, keywords=keywords, location=location)
     process.start()
     send_data()
